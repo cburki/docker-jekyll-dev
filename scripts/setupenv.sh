56c2034852a8ef4b7ba134fb56a4f4699b684ce1
Christophe Burki <christophe.burki@gmail.com>
2016-10-03 08:37:46 -0400

    Setup env only once

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+ENV_STATUS_FILE=/opt/setupenv.status
+
+if [ -f ${ENV_STATUS_FILE} ]; then
+    exit 0
+fi
+
 echo 'export GEM_HOME=/usr/local/bundle' >> /root/.bashrc
 echo 'PATH="/usr/local/bundle/bin:$PATH"' >> /root/.bashrc
 if [ -n "${SSH_USER}" ]; then
     echo 'export GEM_HOME=/usr/local/bundle' >> /home/${SSH_USER}/.bashrc
     echo 'PATH="/usr/local/bundle/bin:$PATH"' >> /home/${SSH_USER}/.bashrc
 fi
+
+echo "done" >> ${ENV_STATUS_FILE}
+
+exit 0
